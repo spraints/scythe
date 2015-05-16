7b684b7363a7820383641ba1c4161b81464b5f1c
Matt Burke <spraints@gmail.com>
2015-05-16 19:59:15 -0400

    gah parser generators the bane of my existance

--- src/logfile.js
+++ src/logfile.js
@@ -1,15 +1,51 @@
-function LogFile(path) {
-  var fields;
-  var records;
-  var loaded = false;
+var fs = require("fs");
+var Parser = require("jison").Parser;
 
-  function _load() {
-    // todo
+var grammar = {
+  lex: {
+    rules: [
+      ["[ \\t]", "/* skip space */"],
+      ["[\\r\\n]+", "return 'EOL';"],
+      ["[^=\\s]+", "return 'TOKEN';"],
+      ["=", "return 'EQL'"]
+    ]
+  },
+
+  bnf: {
+    "lines": [ "lines line", "" ],
+    "line": [ "fields EOL" ],
+    "fields": [ "fields field", "field" ],
+    "field": [ "TOKEN EQL value", "TOKEN" ],
+    "value": [ "TOKEN", "TOKEN EQL", "TOKEN EQL value" ]
   }
+};
 
-  Object.defineProperty(this, "path", { get: function() { return path; } });
-  Object.defineProperty(this, "fields", { get: function() { _load(); return fields; } });
-  Object.defineProperty(this, "records", { get: function() { _load(); return records; } });
+var parser = new Parser(grammar)
+
+function LogFile(path, cb) {
+  var raw = fs.readFileSync(path, "binary");
+  console.log(parser.parse(raw));
+  // var reader = fs.createReadStream(path);
+  // var state = START_LINE;
+  // var buffer;
+  // reader.on("data", function(chunk) {
+  //   while (chunk.length > 0) {
+  //     if (chunk[0] == " " || chunk[0] == "\t") {
+  //       if (state == VALUE) {}
+  //     }
+  //   }
+  //   buffer = buffer + chunk;
+  //
+  // });
+  // reader.resume();
+
+  // Object.defineProperty(this, "path", { get: function() { return path; } });
+  // Object.defineProperty(this, "fields", { get: function() { return fields; } });
+  // Object.defineProperty(this, "records", { get: function() { return records; } });
+}
+
+function LogLine(str) {
+  return str;
 }
 
 module.exports = LogFile;
---
+++ testlogfile.js
@@ -0,0 +1,4 @@
+var LogFile = require("./src/logfile");
+
+lf = new LogFile("../../github/porter/log/scrolls.log");
+console.log(lf);
