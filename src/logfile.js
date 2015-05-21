0af1cc23fcb299305d88c3fd86bbb04c4fd7219f
Matt Burke <spraints@gmail.com>
2015-05-21 08:25:40 -0400

    Start a manual parser for logfiles

--- src/logfile.js
+++ src/logfile.js
@@ -1,43 +1,31 @@
 var fs = require("fs");
-var Parser = require("jison").Parser;
 
-var grammar = {
-  lex: {
-    rules: [
-      ["[ \\t]", "/* skip space */"],
-      ["[\\r\\n]+", "return 'EOL';"],
-      ["[^=\\s]+", "return 'TOKEN';"],
-      ["=", "return 'EQL'"]
-    ]
-  },
-
-  bnf: {
-    "lines": [ "lines line", "" ],
-    "line": [ "fields EOL" ],
-    "fields": [ "fields field", "field" ],
-    "field": [ "TOKEN EQL value", "TOKEN" ],
-    "value": [ "TOKEN", "TOKEN EQL", "TOKEN EQL value" ]
-  }
-};
-
-var parser = new Parser(grammar)
 
 function LogFile(path, cb) {
-  var raw = fs.readFileSync(path, "binary");
-  console.log(parser.parse(raw));
-  // var reader = fs.createReadStream(path);
-  // var state = START_LINE;
-  // var buffer;
-  // reader.on("data", function(chunk) {
-  //   while (chunk.length > 0) {
-  //     if (chunk[0] == " " || chunk[0] == "\t") {
-  //       if (state == VALUE) {}
-  //     }
-  //   }
-  //   buffer = buffer + chunk;
-  //
-  // });
-  // reader.resume();
+  var reader = fs.createReadStream(path);
+  var state = START_LINE;
+  var buffer;
+  reader.on("data", function(chunk) {
+    // todo - finish "record" if unquoted \r or \n
+    for (var i = 0; i < chunk.length; i++) {
+      var ch = chunk[i];
+      if (state == START) {
+        if (ch.match(/\S/)) {
+          buffer = ch;
+          state = KEY;
+        }
+      } else if (state == KEY) {
+        // go until a "="
+      } else if (state == EQL) {
+        // switch to value, quoted-value, or back to START
+      } else if (state == VALUE) {
+        // go until whitespace
+      } else if (state == QUOTED VALUE) {
+        // go until unescaped quote
+      }
+    }
+  });
+  reader.resume();
 
   // Object.defineProperty(this, "path", { get: function() { return path; } });
   // Object.defineProperty(this, "fields", { get: function() { return fields; } });
